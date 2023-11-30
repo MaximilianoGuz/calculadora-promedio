ac5f41d095449f9b9de234a7d0c3cbc0aede2628
MaxG <u20221203934@usco.edu.co>
2023-11-30 15:15:04 -0500

    Estilizado de la página y modificación de los inputs dinámicos

--- js/index.js
+++ js/index.js
@@ -1,54 +1,64 @@
-formToGetData = document.getElementById("form-data");
-formContainer = formToGetData.firstElementChild;
+var formToGetData = document.getElementById("form-data");
+var formContainer = formToGetData.firstElementChild;
 
 createNewImputs();
 
 function createNewImputs() {
-  const divContainer = document.createElement("div");
-  divContainer.className = "container-matter";
+  const trContainer = document.createElement("tr");
+  trContainer.className = "container-matter";
 
   items = document.querySelectorAll(".container-matter").length;
 
-  const inputName = document.createElement("input");
-  inputName.setAttribute("type", "text");
-  inputName.setAttribute("name", `matter-name${items + 1}`);
-  inputName.setAttribute("class", "matter-name");
-  inputName.setAttribute("placeholder", "Materia");
+  const tdCredits = document.createElement("td");
 
   const inputCredits = document.createElement("input");
   inputCredits.setAttribute("type", "number");
   inputCredits.setAttribute("name", `matter-credits${items + 1}`);
   inputCredits.setAttribute("class", "matter-credits");
   inputCredits.setAttribute("placeholder", "Numero de creditos");
+  inputCredits.setAttribute("required", "true");
+
+  tdCredits.appendChild(inputCredits);
+
+  const tdNote = document.createElement("td");
 
   const inputNote = document.createElement("input");
   inputNote.setAttribute("type", "number");
   inputNote.setAttribute("name", `matter-note${items + 1}`);
   inputNote.setAttribute("class", "matter-note");
   inputNote.setAttribute("placeholder", "Nota");
+  inputNote.setAttribute("required", "true");
+
+  tdNote.appendChild(inputNote);
+
+  const tdButtons = document.createElement("td");
 
   const addButtonNew = document.createElement("button");
   addButtonNew.setAttribute("type", "button");
   addButtonNew.setAttribute("class", "add-matter");
-  addButtonNew.textContent = "+";
+  addButtonNew.innerHTML = `<i class="fa-solid fa-plus"></i>`;
 
   const deleteButtonNew = document.createElement("button");
   deleteButtonNew.setAttribute("type", "button");
   deleteButtonNew.setAttribute("class", "delete-matter");
-  deleteButtonNew.textContent = "X";
+  deleteButtonNew.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
 
-  divContainer.appendChild(inputName);
-  divContainer.appendChild(inputCredits);
-  divContainer.appendChild(inputNote);
-  divContainer.appendChild(addButtonNew);
-  divContainer.appendChild(deleteButtonNew);
-
-  formContainer.appendChild(divContainer);
-
-  addButtonNew.addEventListener("click", createNewImputs);
+  addButtonNew.addEventListener("click", (e) => {
+    if (document.querySelectorAll(".container-matter").length <= 19)
+      createNewImputs();
+  });
 
   deleteButtonNew.addEventListener("click", (e) => {
     if (document.querySelectorAll(".container-matter").length > 1)
-      formContainer.removeChild(e.target.parentElement);
+      formContainer.removeChild(trContainer);
   });
+
+  tdButtons.appendChild(addButtonNew);
+  tdButtons.appendChild(deleteButtonNew);
+  
+  trContainer.appendChild(tdCredits);
+  trContainer.appendChild(tdNote);
+  trContainer.appendChild(tdButtons);
+
+  formContainer.appendChild(trContainer);
 }
